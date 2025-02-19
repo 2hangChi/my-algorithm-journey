769ec247b04c3ffbb8c8ece50e15f401e43da5a0
ZhangChi <e1221818@u.nus.edu>
2025-02-19 09:42:58 +0800

    modify 046

--- src/class046/lc1371.java
+++ src/class046/lc1371.java
@@ -10,7 +10,8 @@ public class lc1371 {
             map.put(0, -1);
             int ans = 0;
             for (int i = 0, state = 0; i < s.length(); i++) {
-                state ^= char2path(s.charAt(i));
+                state ^= char2path(s.charAt(i)); // 函数返回1左移的位数的话还需要加条件判断default时不左移
+                // 这样直接返回1，2，4，8，16和0恰好不用判断，应该是比较好的方式
                 if (map.containsKey(state)) {
                     ans = Math.max(ans, i - map.get(state));
                 } else {
@@ -32,13 +33,13 @@ public class lc1371 {
         }
     }
 
-    class Solution1 {
+    class Solution1 { // 没必要用HashMap
         public int findTheLongestSubstring(String s) {
             int[] map = new int[32];
             Arrays.fill(map, -2);
             map[0] = -1;
             int ans = 0;
-            for (int i = 0, state = 0; i < s.length(); i++) {
+            for (int i = 0, state = 0; i < s.length(); i++) { // i<s.length() 换成 提前取n=s.length()然后i<n 会快2ms
                 state ^= char2path(s.charAt(i));
                 if (map[state] != -2) {
                     ans = Math.max(ans, i - map[state]);
